3bfd50840b7e8fdc842c50c835e8585839072b19
CoderTejas <75924121+CoderTejas@users.noreply.github.com>
2021-09-26 23:15:20 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center()
+}
+
+function draw()
+{
+
+}
+
+function save_img()
+{
+    save('my_filter.png');
+}
